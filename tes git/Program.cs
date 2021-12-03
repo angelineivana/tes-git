27e318d9726c15711371a657ecd3713dca170746
LENOVO <LENOVO@LAPTOP-EFTDPVT1>
2021-12-03 09:51:45 +0700

    Add project files.

---
+++ tes git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tes_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
